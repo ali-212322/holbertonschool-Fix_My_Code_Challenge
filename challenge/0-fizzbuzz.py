241797149dc5a133c9d40006b007f9139d8941f2
Ali Hassan <alimalmaghrabi@gmail.com>
2026-05-02 19:46:39 +0300

    task1

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -1,30 +1,39 @@
 #!/usr/bin/python3
-"""FizzBuzz implementation"""
+""" FizzBuzz
+"""
+import sys
+
 
 def fizzbuzz(n):
     """
-    Print numbers from 1 to n, but:
-    - print "Fizz" if divisible by 3
-    - print "Buzz" if divisible by 5
-    - print "FizzBuzz" if divisible by both 3 and 5
-    """
-    result = []
-    for i in range(1, n + 1):
-        if i % 15 == 0:
-            result.append("FizzBuzz")
-        elif i % 3 == 0:
-            result.append("Fizz")
-        elif i % 5 == 0:
-            result.append("Buzz")
-        else:
-            result.append(str(i))
-    return " ".join(result)
+    FizzBuzz function prints numbers from 1 to n separated by a space.
 
-if __name__ == "__main__":
-    import sys
+    - For multiples of three print "Fizz" instead of the number and for
+      multiples of five print "Buzz".
+    - For numbers which are multiples of both three and five print "FizzBuzz".
+    """
+    if n < 1:
+        return
+
+    tmp_result = []
+    for i in range(1, n + 1):
+        if (i % 3) == 0 and (i % 5) == 0:
+            tmp_result.append("FizzBuzz")
+        elif (i % 3) == 0:
+            tmp_result.append("Fizz")
+        elif (i % 5) == 0:
+            tmp_result.append("Buzz")
+        else:
+            tmp_result.append(str(i))
+    print(" ".join(tmp_result))
+
+
+if __name__ == '__main__':
     if len(sys.argv) <= 1:
+        print("Missing number")
         print("Usage: ./0-fizzbuzz.py <number>")
+        print("Example: ./0-fizzbuzz.py 89")
         sys.exit(1)
-    
+
     number = int(sys.argv[1])
-    print(fizzbuzz(number))
+    fizzbuzz(number)
